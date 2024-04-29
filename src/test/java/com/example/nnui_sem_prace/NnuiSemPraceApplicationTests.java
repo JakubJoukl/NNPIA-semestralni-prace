f9e4ea6aca20cf555f7495fe244be4e6e175cb69
JakubJoukl <st64134@upce.cz>
2024-04-29 14:46:31 +0200

    Filtrovani + oprava unit testu

--- src/test/java/com/example/nnui_sem_prace/NnuiSemPraceApplicationTests.java
+++ src/test/java/com/example/nnui_sem_prace/NnuiSemPraceApplicationTests.java
@@ -1,14 +1,42 @@
 package com.example.nnui_sem_prace;
 
+import com.example.nnui_sem_prace.service.RezervaceTerminuService;
+import com.example.nnui_sem_prace.service.UzivatelService;
+import com.example.nnui_sem_prace.service.VypsaneTerminyService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+        classes = NnuiSemPraceApplication.class)
+@AutoConfigureMockMvc
 class NnuiSemPraceApplicationTests {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private RezervaceTerminuService rezervaceTerminuService;
+
+    @Autowired
+    private UzivatelService uzivatelService;
+
+    @Autowired
+    private VypsaneTerminyService vypsaneTerminyService;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Autowired
+    JwtDecoder jwtDecoder;
 
     @Test
     void contextLoads() {
 
     }
-
 }
